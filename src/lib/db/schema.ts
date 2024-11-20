e0aa05ea5e2583c3ee122525ad4301fcec0b9be2
Pankaj Kumar <94382374+Py8050147@users.noreply.github.com>
2024-11-20 23:37:01 +0530

    "Added new tables to database schema: teams, projects, tasks, comments, and attachments; updated _journal.json with new entries"

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,5 @@
 import { sql } from "drizzle-orm";
-import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text,  timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
     id: serial('id').primaryKey(),
@@ -9,6 +9,82 @@ export const users = pgTable('users', {
     password: varchar('password', { length: 100 }).notNull(),
     image: text('image'),
     role: varchar('role', { length: 12 }).notNull().default('student'), 
+    updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
+})
+
+
+export const teams = pgTable('teams', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 100 }).notNull(),
+    description: text('description'),
+    updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
+})
+
+export const projects = pgTable('projects', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 500 }).notNull(),
+    description: text('description'),
+    userId: integer('user_id').notNull().references(() => users.id, { onDelete: 'cascade' }).notNull(),
+    teamId: integer('team_id').notNull().references(() => teams.id, { onDelete: 'no action' }).notNull(),
+    startDate: timestamp('start_date').notNull(),
+    endDate: timestamp('end_date'),
+    status: varchar('status', { length: 10 }).notNull().default('Archived'),
+    updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
+})
+
+export enum Status {
+    ToDo = "To-Do",
+    InProgress = "In Progress",
+    Completed = "Completed",
+}
+
+export enum Priority {
+    Low = "Low",
+    Medium = "Medium",
+    High = "High",
+}
+
+
+export const tasks = pgTable('tasks', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 500 }).notNull(),
+    description: text('description'),
+    projectid: integer('project_id').notNull().references(() => projects.id,  {
+      onDelete: 'set null'
+    }).notNull(),
+    userId: integer('user_id').notNull().references(() => users.id,  {
+        onDelete: 'cascade'
+    }).notNull(),
+    status: varchar("status", { length: 20 }).$type<Status>().notNull(), // Enum enforced at TypeScript level
+    priority: varchar("priority", { length: 10 }).$type<Priority>().notNull(), // Enum enforced at TypeScript level
+    dueDate: timestamp('due_date'),
+    updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
+})
+
+export const comments = pgTable('comments', {
+    id: serial('id').primaryKey(),
+    taskId: integer('task_id').notNull().references(() => tasks.id, {
+        onDelete: 'set null'
+    }).notNull(),
+    userId: integer('user_id').notNull().references(() => users.id,  {
+        onDelete: 'cascade'
+    }).notNull(),
+    text: varchar('text', { length: 500 }),
+    updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
+    createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
+})
+
+export const attachments = pgTable('attachments', {
+    id: serial('id').primaryKey(),
+    taskId: integer('task_id').notNull().references(() => tasks.id, {
+        onDelete: 'set null'
+    }).notNull(),
+    fileName: varchar('fileName', { length: 500 }).notNull(),
+    fileUrl: text('file_url').notNull(),
     updatedAt: timestamp('updated_at').default(sql`CURRENT_TIMESTAMP`),
     createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`)
 })
